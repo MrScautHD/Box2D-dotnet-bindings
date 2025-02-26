83f26acc4b89b36ad922537b636e9708bd1d0ede
Hugh Phoenix-Hulme <hugh@webnet.org.uk>
2025-02-26 23:21:58 +0000

    Added missing files

---
+++ UnitTests/CreationTests.cs
@@ -0,0 +1,44 @@
+using Box2D;
+
+namespace TestProject1;
+
+public class CreationTests
+{
+    [Fact]
+    public void CreateWorldFromDefault()
+    {
+        WorldDef def = WorldDef.Default;
+        World world = World.CreateWorld(def);
+    }
+    
+    [Fact]
+    public void CreateWorldDefFromNew()
+    {
+        WorldDef def = new WorldDef();
+        WorldDef fromDefault = WorldDef.Default;
+        Assert.Equal(def.MaximumLinearSpeed, fromDefault.MaximumLinearSpeed);
+    }
+    
+    [Fact] void CreateTwoJointedBodies()
+    {
+        WorldDef worldDf = new WorldDef();
+        World world = World.CreateWorld(worldDf);
+        
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.Type = BodyType.Dynamic;
+        bodyDef.Position = (-10f, 0f);
+        Body bodyA = world.CreateBody(bodyDef);
+        
+        bodyDef.Position = (10f, 0f);
+        Body bodyB = world.CreateBody(bodyDef);
+        
+        DistanceJointDef jointDef = new DistanceJointDef();
+        jointDef.BodyA = bodyA;
+        jointDef.BodyB = bodyB;
+        jointDef.LocalAnchorA = (0f, 0f);
+        jointDef.LocalAnchorB = (0f, 0f);
+        
+        Joint joint = world.CreateJoint(jointDef);
+        
+    }
+}
